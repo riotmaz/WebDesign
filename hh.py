37caf65d292373c5d0d279b57b275c39bb17323f
unknown <riobros1234@gmail.com>
2024-11-11 11:20:47 +0530

    Table new commit

---
+++ hh.py
@@ -0,0 +1,43 @@
+_nq=list((input('').split()))
+print('fgfgfg')
+print(_nq)
+while True:
+    ai=list((input('').split()))
+    if len(ai)==int(_nq[0]):
+        break
+    print('ENTER THE CORRECT NUMBE OF AI')
+while True:
+    bi=list((input('').split()))
+    if len(bi)==int(_nq[0]):
+        break
+    print('ENTER THE CORRECT NUMBE OF BI')
+queslis=[]
+for i in range(int(_nq[1])):
+    a=int(input(''))
+    queslis.append(a)
+print(queslis)
+a=0
+b=0
+a=max(ai)
+b=max(bi)
+aid=dict(enumerate(ai))
+bid=dict(enumerate(bi))
+a=max(for i aid(i))
+b=max(bid)
+print(aid)
+print(a,b)
+for i in queslis:
+    a=max(ai)
+    b=max(bi)
+
+
+
+
+
+
+
+
+
+
+
+
